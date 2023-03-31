00bc8530af7f81f79edaa2d8f6de54fe1335e811
avive <avive@users.noreply.github.com>
2023-03-31 10:23:07 +0000

    deploy: 05a3db1f7cb2e34e341e3467f84cbf049eea8616

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
-"main.dart.js": "d4b5797bb4c36ecc66d64236413c6956",
+"main.dart.js": "4a1f1ba6b341d9d2a283144c591101f9",
 "canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
 "canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
 "canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
@@ -22,8 +22,8 @@ const RESOURCES = {
 "assets/packages/status_alert/assets/fonts/SFNS.ttf": "9e14b4e72dec1db9a60d2636bbfe64f2",
 "assets/FontManifest.json": "340ba7db15928dafeb99710b23eb301b",
 "assets/NOTICES": "2a366309269a20031094cbb61f72a815",
-"index.html": "aff59be767d094952efcb75a45e0f68f",
-"/": "aff59be767d094952efcb75a45e0f68f",
+"index.html": "4281a151c478c3162fd41e92b75a4b3a",
+"/": "4281a151c478c3162fd41e92b75a4b3a",
 "icons/Icon-512.png": "4d029c2692691ebf213fc8ff646c7f26",
 "icons/Icon-192.png": "7e4cccf108e12a39292d5806c6450f7a",
 "icons/Icon-maskable-512.png": "f790d4c3526e16dd188ea2943da3c93e",
